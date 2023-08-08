e0402e1784d595be090f60ac54b057f4631ea260
Nataliia <134400506+NataliiaZabazhanova@users.noreply.github.com>
2023-08-08 17:24:23 +0100

    Add footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
+import Footer from './Footer';
 import Weather from './Weather';
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather />
+        <div className='App__body'>
+          <Weather />
+        </div>
+        <div className='App__footer'>
+          <Footer />
+        </div>
       </div>
     </div>
   );
---
+++ src/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Footer.css";
+
+export default function Footer() {
+    return (
+        <div className="Footer">
+           <p>Nataliia Zabazhanova   <a href="https://github.com/NataliiaZabazhanova/weather-week4-hw">GitHub</a></p>
+        </div>
+    );
+}
