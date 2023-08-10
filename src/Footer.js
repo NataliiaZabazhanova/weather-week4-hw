84c8c063f3b727238787319791a3b6e2ca386584
Nataliia <134400506+NataliiaZabazhanova@users.noreply.github.com>
2023-08-10 23:20:08 +0100

    upd footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,7 @@ import "./Footer.css";
 export default function Footer() {
     return (
         <div className="Footer">
-           <p>Nataliia Zabazhanova   <a href="https://github.com/NataliiaZabazhanova/weather-week4-hw">GitHub</a></p>
+           <p>Nataliia Zabazhanova   <a href="https://github.com/NataliiaZabazhanova/weather-week4-hw" target="_blank" rel="noreferrer">GitHub</a></p>
         </div>
     );
 }
